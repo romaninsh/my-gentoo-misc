5142d90c874ea77f6ff1b29a3b6cd7f33274b6d9
Romans Malinovskis <romans@agiletoolkit.org>
2013-09-02 21:06:12 +0000

    fixes

--- etc/profile.d/mybin.sh
+++ etc/profile.d/mybin.sh
@@ -1,3 +1,6 @@
-echo "This system uses Romans My-Gentoo-Misc repsoitory"
-echo "https://github.com/romaninsh/my-gentoo-misc"
 export PATH=$PATH:/etc/my-gentoo-misc/bin
+
+alias top=htop
+alias df=pydf
+alias grep='grep --colour=auto'
+alias l='ls -la'
