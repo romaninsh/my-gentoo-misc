5ed014070e76086b18a9efdd938ffe46cafd61a2
Romans Malinovskis <romans@agiletoolkit.org>
2013-09-02 21:04:58 +0000

    fix to path /etc/my

--- etc/profile.d/mybin.sh
+++ etc/profile.d/mybin.sh
@@ -1,3 +1,3 @@
 echo "This system uses Romans My-Gentoo-Misc repsoitory"
 echo "https://github.com/romaninsh/my-gentoo-misc"
-export PATH=$PATH:/etc/my-gentoo-misc/bin
+export PATH=$PATH:/etc/my/bin
